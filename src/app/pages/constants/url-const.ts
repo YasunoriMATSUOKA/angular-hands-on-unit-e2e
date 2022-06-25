9be5553aa173ff1ec6e9e936db0d54350ae135b7
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-25 20:36:36 +0900

    fix: typo of url-const

--- src/app/pages/constants/url-const.ts
+++ src/app/pages/constants/url-const.ts
@@ -5,8 +5,7 @@ export class UrlConst {
   static readonly PATH_PRODUCT_LISTING = 'product-listing';
   static readonly PATH_PRODUCT_REGISTERING = 'product-registering';
   static readonly PATH_PRODUCT_REGISTERING_NEW = 'product-registering/new';
-  static readonly PATH_PRODUCT_PURCHASE_HISTORY_LISTING =
-    'purchase-history-listing';
+  static readonly PATH_PURCHASE_HISTORY_LISTING = 'purchase-history-listing';
   static readonly PATH_STOCK_REGISTERING = 'stock-registering';
   static readonly PATH_DUMMY_PURCHASING = 'dummy-purchasing';
 }
