e5b23185fb2d97e9f7473f5a8737098b635a11c5
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-25 20:24:11 +0900

    feat: pages module related resource scaffolding

---
+++ src/app/pages/components/stock-registering-page/stock-registering-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-registering-page',
+  templateUrl: './stock-registering-page.component.html',
+  styleUrls: ['./stock-registering-page.component.scss'],
+})
+export class StockRegisteringPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
