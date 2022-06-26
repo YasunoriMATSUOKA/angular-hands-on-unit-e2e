b5d6e5f9b61b5596d8026fe700877fe04d3c2664
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-06-26 11:46:00 +0900

    feat: super user related resources scaffolding and set routing

---
+++ src/app/super-user-pages/dummy-purchasing-page/dummy-purchasing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy-purchasing-page',
+  templateUrl: './dummy-purchasing-page.component.html',
+  styleUrls: ['./dummy-purchasing-page.component.scss'],
+})
+export class DummyPurchasingPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
