5abba53f87c67729bcc44419bfe9a4710108d30c
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-07-02 18:51:26 +0900

    test: core service

--- src/app/core/services/error-messaging.service.spec.ts
+++ src/app/core/services/error-messaging.service.spec.ts
@@ -1,8 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ErrorMessagingService } from './error-messaging.service';
 
-xdescribe('ErrorMessagingService', () => {
+describe('ErrorMessagingService', () => {
   let service: ErrorMessagingService;
 
   beforeEach(() => {
@@ -10,7 +11,104 @@ xdescribe('ErrorMessagingService', () => {
     service = TestBed.inject(ErrorMessagingService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#getMessageProperty, #setMessageProperty', () => {
+    it('should return expected response', () => {
+      const expectedValue = 'message';
+      service.setMessageProperty(expectedValue);
+      expect(service.getMessageProperty()).toEqual(expectedValue);
+    });
+  });
+
+  describe('#clearMessageProperty', () => {
+    it('should clear property', () => {
+      const expectedValue = 'message';
+      service.setMessageProperty(expectedValue);
+      expect(service.getMessageProperty()).toEqual(expectedValue);
+      service.clearMessageProperty();
+      expect(service.getMessageProperty()).toEqual('');
+    });
+  });
+
+  describe('#setupPageErrorMessageFromResponse', () => {
+    const testCases = [
+      {
+        description: 'should set property when error status is 400',
+        error: { status: 400 },
+        expectedValue: 'errMessage.http.badRequest',
+      },
+      {
+        description: 'should set property when error status is 401',
+        error: { status: 401 },
+        expectedValue: 'errMessage.http.unAuthorized',
+      },
+      {
+        description: 'should set property when error status is 404',
+        error: { status: 404 },
+        expectedValue: 'errMessage.http.notFound',
+      },
+      {
+        description:
+          'should set property when error status is 500 _ Duplicated key.',
+        error: { status: 500, error: { message: 'Duplicated key.' } },
+        expectedValue: 'errMessage.http.duplicateKeyException',
+      },
+      {
+        description:
+          'should set property when error status is 500 _ Exclusive error occurred.',
+        error: { status: 500, error: { message: 'Exclusive error occurred.' } },
+        expectedValue: 'errMessage.http.exclusiveProcessingException',
+      },
+      {
+        description:
+          'should set property when error status is 500 _ There is no stock.',
+        error: { status: 500, error: { message: 'There is no stock.' } },
+        expectedValue: 'errMessage.http.outOfStockException',
+      },
+      {
+        description:
+          'should set property when error status is 500 _ Data not found.',
+        error: { status: 500, error: { message: 'Data not found.' } },
+        expectedValue: 'errMessage.http.datNotFoundException',
+      },
+      {
+        description:
+          'should set property when error status is 500 _ Another message',
+        error: { status: 500, error: { message: 'Another message' } },
+        expectedValue: 'errMessage.http.internalServerError',
+      },
+      {
+        description: 'should set property when error status is 501',
+        error: { status: 501 },
+        expectedValue: 'errMessage.http.GenericError',
+      },
+      {
+        description:
+          "should set property when error is instanceof Error _ 'Unknown error'",
+        error: { status: undefined, message: 'Unknown error' },
+        expectedValue: 'errMessage.common.unknown',
+      },
+    ];
+
+    testCases.forEach((testCase) => {
+      it(testCase.description, () => {
+        let error: HttpErrorResponse | Error;
+        if (testCase.error.status) {
+          error = new HttpErrorResponse({
+            status: testCase.error.status,
+            error: new Error(testCase.error.error?.message),
+          });
+        } else {
+          error = new Error(testCase.error.message);
+        }
+        service.setupPageErrorMessageFromResponse(error);
+        expect(service.getMessageProperty()).toEqual(testCase.expectedValue);
+      });
+    });
   });
 });
--- src/app/core/services/session-storage.service.spec.ts
+++ src/app/core/services/session-storage.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
+import { AppConst } from 'src/app/pages/constants/app-const';
+import { User } from 'src/app/pages/models/user';
 
 import { SessionStorageService } from './session-storage.service';
 
-xdescribe('SessionStorageService', () => {
+function createUser(): User {
+  return new User({
+    userAccount: 'userAccount',
+    userName: 'userName',
+    userLocale: 'ja-JP',
+    userLanguage: 'ja',
+    userTimezone: 'Asia/Tokyo',
+    userTimezoneOffset: '+09:00',
+    userCurrency: 'JPY',
+  });
+}
+
+describe('SessionStorageService', () => {
   let service: SessionStorageService;
+  let expectedUser: User;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionStorageService);
+    expectedUser = createUser();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#setItem, #getItem', () => {
+    it('should set and get item', () => {
+      SessionStorageService.setItem<User>(
+        AppConst.STORAGE_KEY_USER,
+        expectedUser
+      );
+      const resultUser = SessionStorageService.getItem<User>(
+        AppConst.STORAGE_KEY_USER
+      );
+      expect(resultUser.userAccount).toEqual(expectedUser.userAccount);
+      expect(resultUser.userName).toEqual(expectedUser.userName);
+      expect(resultUser.userLocale).toEqual(expectedUser.userLocale);
+      expect(resultUser.userLanguage).toEqual(expectedUser.userLanguage);
+      expect(resultUser.userTimezone).toEqual(expectedUser.userTimezone);
+      expect(resultUser.userTimezoneOffset).toEqual(
+        expectedUser.userTimezoneOffset
+      );
+      expect(resultUser.userCurrency).toEqual(expectedUser.userCurrency);
+    });
+  });
+
+  describe('#removeItem', () => {
+    it('should remove item', () => {
+      SessionStorageService.setItem<User>(
+        AppConst.STORAGE_KEY_USER,
+        expectedUser
+      );
+      SessionStorageService.removeItem(AppConst.STORAGE_KEY_USER);
+      const errorFunction = () => {
+        SessionStorageService.getItem<User>(AppConst.STORAGE_KEY_USER);
+      };
+      expect(errorFunction).toThrow(new Error('key USER not found!'));
+    });
   });
 });
