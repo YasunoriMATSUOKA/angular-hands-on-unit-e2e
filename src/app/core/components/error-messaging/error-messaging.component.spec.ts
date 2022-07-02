70a23d83c40b6a6395b401d81815c955ca609fc9
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-07-02 19:45:50 +0900

    test: core components

--- src/app/core/components/error-messaging/error-messaging.component.spec.ts
+++ src/app/core/components/error-messaging/error-messaging.component.spec.ts
@@ -1,13 +1,35 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { TranslateTestingModule } from 'ngx-translate-testing';
+import { ErrorMessagingService } from '../../services/error-messaging.service';
 
 import { ErrorMessagingComponent } from './error-messaging.component';
 
-xdescribe('ErrorMessagingComponent', () => {
+describe('ErrorMessagingComponent', () => {
   let component: ErrorMessagingComponent;
   let fixture: ComponentFixture<ErrorMessagingComponent>;
+  let errorMessagingServiceSpy: {
+    clearMessageProperty: jasmine.Spy;
+    getMessageProperty: jasmine.Spy;
+  };
 
   beforeEach(async () => {
+    errorMessagingServiceSpy = jasmine.createSpyObj('ErrorMessagingService', [
+      'clearMessageProperty',
+      'getMessageProperty',
+    ]);
+
     await TestBed.configureTestingModule({
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        TranslateTestingModule.withTranslations({
+          ja: require('src/assets/i18n/ja.json'),
+        }),
+      ],
+      providers: [
+        { provide: ErrorMessagingService, useValue: errorMessagingServiceSpy },
+      ],
       declarations: [ErrorMessagingComponent],
     }).compileComponents();
 
@@ -16,7 +38,32 @@ xdescribe('ErrorMessagingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('#constructor', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+  });
+
+  describe('#ngOnInit', () => {
+    it('should init', () => {
+      component.ngOnInit();
+      expect(
+        errorMessagingServiceSpy.clearMessageProperty.calls.count()
+      ).toBeGreaterThan(1);
+    });
+
+    it('should display error message', () => {
+      const errorMessageProperty = 'errMessage.http.badRequest';
+      const expectedValue = '入力情報が正しくありません。';
+
+      errorMessagingServiceSpy.getMessageProperty.and.returnValue(
+        errorMessageProperty
+      );
+      fixture.detectChanges();
+
+      const htmlParagraphElement: HTMLParagraphElement =
+        fixture.debugElement.query(By.css('p')).nativeElement;
+      expect(htmlParagraphElement.innerText).toEqual(expectedValue);
+    });
   });
 });
