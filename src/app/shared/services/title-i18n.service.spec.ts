598a806438fd8ca7f76ba9525cfefbfe3e59dcf8
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2022-07-02 19:57:15 +0900

    test: shared services

--- src/app/shared/services/title-i18n.service.spec.ts
+++ src/app/shared/services/title-i18n.service.spec.ts
@@ -1,16 +1,38 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateTestingModule } from 'ngx-translate-testing';
 
 import { TitleI18nService } from './title-i18n.service';
 
-xdescribe('TitleI18nService', () => {
+describe('TitleI18nService', () => {
   let service: TitleI18nService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateTestingModule.withTranslations({
+          ja: require('src/assets/i18n/ja.json'),
+        }),
+      ],
+    });
     service = TestBed.inject(TitleI18nService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('#constructor', () => {
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+  });
+
+  describe('#setTitle', () => {
+    it('should set title', () => {
+      const screenName = 'sign-in';
+      const expectedTitleSystem = '【Example Site】';
+      const expectedTitleSub = 'ログイン';
+
+      service.setTitle(screenName);
+      expect(service.title.getTitle()).toBe(
+        expectedTitleSystem + expectedTitleSub
+      );
+    });
   });
 });
